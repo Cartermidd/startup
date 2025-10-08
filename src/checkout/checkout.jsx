969b71e09bbe52ea9b639300c3fb453a3d75d564
Carter Middleton <carterwmiddleton@gmail.com>
2025-10-08 10:01:30 -0600

    checkout.jsx done

--- src/checkout/checkout.jsx
+++ src/checkout/checkout.jsx
@@ -4,7 +4,27 @@ import React from 'react';
 export function Checkout() {
     return (
         <main className='container-fluid bg-primary text-center'>
-            <div>Checkout components</div>
+            <div>
+                <h1>CHECK OUT</h1>
+
+                <h2>Welcome, [username]!</h2>
+
+                <p>
+                    Abreviated version of Cart
+                    (websocket goes here)
+                </p>
+
+                <div id="price-n-pay-box">
+                    <h2>Total Price: $ [price_variable]</h2>
+                    <button type="submit" className="btn btn-primary btn-sm">Purchase</button>
+                </div>
+
+                <div id="shipping-info">
+                    <h2>API shipping information</h2>
+                    <img width="200px" src="" alt="Some sort of shipping display" />
+                </div>
+ 
+            </div>
         </main>
     )
 }
