65c4b146471656448cde713f453cac8e9a54d574
Carter Middleton <carterwmiddleton@gmail.com>
2025-10-22 17:44:29 -0600

    Adding Product Dataframes

---
+++ src/products/productData.jsx
@@ -0,0 +1,58 @@
+
+
+
+const productData = [
+    {
+        id: wooden-1,
+        name: "Wooden Placeholder 1",
+        description: "This is a placeholder description for Wooden Product 1.",
+        image: "/rockcanyon_design.png",
+        category: "Wooden"
+    },
+
+    {
+        id: wooden-2,
+        name: "Wooden Placeholder 2",
+        description: "This is a placeholder description for Wooden Product 2.",
+        image: "/rockcanyon_design.png",
+        category: "Wooden"
+    },
+    {
+        id: acrylic-1,
+        name: "Acrylic Placeholder 1",
+        description: "This is a placeholder description for Acrylic Product 1.",
+        image: "/rockcanyon_design.png",
+        category: "Acrylic"
+    },
+    
+    {
+        id: acrylic-2,
+        name: "Acrylic Placeholder 2",
+        description: "This is a placeholder description for Acrylic Product 2.",
+        image: "/rockcanyon_design.png",
+        category: "Acrylic"
+    },
+
+    {
+        id: giftcardholder-1,
+        name: "Giftcard Holder Placeholder 1",
+        description: "This is a placeholder description for Giftcard Holder Product 1.",
+        image: "/rockcanyon_design.png",
+        category: "Giftcard Holder"
+    },
+    {
+        id: giftcardholder-2,
+        name: "Giftcard Holder Placeholder 2",
+        description: "This is a placeholder description for Giftcard Holder Product 2.",
+        image: "/rockcanyon_design.png",
+        category: "Giftcard Holder"
+    },
+    {
+        id: other-1,
+        name: "Other Placeholder 1",
+        description: "This is a placeholder description for Other Product 1.",
+        image: "/rockcanyon_design.png",
+        category: "Other"
+    }
+];
+
--- src/products/products.jsx
+++ src/products/products.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+
+
+
 
 
 export function Products() {
